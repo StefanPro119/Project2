8d106663892c9e2c49d08e095da017298637a25e
StefanPro119 <74908361+StefanPro119@users.noreply.github.com>
2021-05-13 10:20:28 +0200

    creating models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,34 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+#klasa za prodavanje proizvoda
+class Auction(models.Model):
+    seller = models.CharField(max_length=64)
+    title = models.CharField(max_length=60)
+    subtitle = models.CharField(max_length=120)
+    start_bit = models.IntegerField()
+    image = models.ImageField()
+    data = models.DateTimeField(auto_now=True)
+    # select_category =
+
+    def __str__(self):
+        return f"{self.seller} is selling {self.title}"
+
+
+#klaza za licitiranje proizvoda
+class Bid(models.Model):
+    buyer = models.CharField(max_length=64)
+    bid_value = models.IntegerField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.buyer} is offering {self.bid_value}"
+
+#klasa za komentarisanje proizvoda
+class Comment(models.Model):
+    commments = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.commments}"
